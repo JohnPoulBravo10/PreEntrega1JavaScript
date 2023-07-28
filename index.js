6f5c17ad9fa118714d614e20f46c847e56a13010
juan pablo bravo <juanpablobravo0209@gmail.com>
2023-07-28 11:55:21 -0300

    entrega 2

--- index.js
+++ index.js
@@ -1,75 +1,193 @@
 alert("Bienvenido a nuestra tienda");
 alert("A continuacion se le mostrara nuestros productos en stock");
 
-let numeroIngresado;
-let precio=0;
-let agregar;
-let cantidadProductos=0;
+class Producto{
 
-function mostrarMenuProductos(){
+    constructor(id,categoria,nombre,precio=0,stock=0){
+        this.id=id;
+        this.categoria=categoria;
+        this.nombre=nombre;
+        this.precio=precio;
+        this.stock=stock;
+    }
+    setPrecio(precio){
+        this.precio=precio;
+    }
+    setPrecio(nombre){
+        this.nombre=nombre;
+    }
+    setPrecio(stock){
+        this.stock=stock;
+    }
+    toString() {
+        return "IdProducto: " + this.id + "\nNombre :" + this.nombre + "\nPrecio: $" + this.precio +"\nStock: " + this.stock;
+    }
 
-    while(numeroIngresado!="SALIR"){
-        
-        numeroIngresado=prompt("Que producto desea comprar, ingrese una opcion: \n -1: Campera Puffer $34000 \n -2: Buzo Palace $15250 \n -3: Buzo Currentrly $13542 \n -4: Chaleco Puffer $23433\n Ingrese SALIR para ir al carrito de compras");
-        
-        if(numeroIngresado!="SALIR"){
-            agregar=prompt("Desea agregarlo a su carrito de comprar? \n Ingrese si para confirmar\n Ingrese no para volver a visualizar los productos en stock");
-            cantidadProductos++;
-            if(agregar=="si"){
-                switch(numeroIngresado){
-                    case "1":
-                        precio+=34000;
-                        alert("Producto agregado al carrito");
-                        break;
-                    case "2":
-                        precio+=15250;
-                        alert("Producto agregado al carrito");
-                        break;
-                    case "3":
-                        precio+=13542;
-                        alert("Producto agregado al carrito");
-                        break;
-                    case "4":
-                        precio+=23433;
-                        alert("Producto agregado al carrito");
-                        break;
-                    default: alert("El valor ingresado es incorrecto");
-                }
-            }
-       
-        }
-       
+    calcularCuotas(cuotas){
+        alert("El precio a pagar es "+(this.precio/cuotas)+" en "+ cuotas +" cuotas");
     }
 
 }
+let productos = [
+    new Producto(1,"Camperas","Campera Puffer", 34000, 5),
+    new Producto(2,"Camperas","Campera Rompe Viento", 23000, 5),
+    new Producto(3,"Camperas","Campera de Cuero", 27800, 5),
+    new Producto(4,"Buzos","Buzo Palace", 15250, 2),
+    new Producto(5,"Buzos","Buzo Currentrly", 13542, 3),
+    new Producto(6,"Buzos","Buzo Oversize", 13542, 3),
+    new Producto(7,"Chalecos","Chaleco Puffer negro", 23433, 1),
+    new Producto(8,"Chalecos","Chaleco Puffer azul oscuro", 23433, 1),
+    new Producto(9,"Chalecos","Chaleco Puffer rojo", 23433, 1)
+  ];
 
-function calcularCuotas(precio,cuotas){
-    alert("El precio a pagar es "+(precio/cuotas)+" en "+cuotas+" cuotas");
+let carrito = [];
+
+function mostrarProductos(arregloProductos,categoria){
+    console.clear();
+    console.log("Productos");
+
+  arregloProductos.sort((a, b) => {
+    if (a.nombre > b.nombre) {
+      return 1;
+    } else if (a.nombre < b.nombre) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  if(categoria !== "carrito"){
+    let productoPorCategoria = arregloProductos.filter(producto => producto.categoria === categoria);
+    productoPorCategoria.forEach( producto => console.log(producto.toString()));
+  }else{
+    arregloProductos.forEach(producto => console.log(producto.toString()));
+  }
+  
+  
+};
+
+
+
+function mostrarMenuProductos(categoria){
+
+    mostrarProductos(productos,categoria);
+
+    do{
+      idProductoIngresado = prompt("Que producto desea comprar, ingrese id del Producto o ingrese Volver para ir al menu de categorias");
+        
+      let producto = productos.find(producto => producto.id === parseInt(idProductoIngresado));
+      
+      if(idProductoIngresado.toUpperCase() != "VOLVER" && producto.categoria === categoria){
+
+          const confirmacion = confirm(`Estas seguro que deseas agregar el producto ${producto.nombre} al carrito?`)
+            
+          if(confirmacion){
+
+                carrito.push(producto);
+
+                alert(`Producto ${producto.nombre} agregado al carrito`);
+
+          }
+        
+        }else if(idProductoIngresado.toUpperCase() != "VOLVER"){
+          alert("Por favor ingrese una opcion correcta");
+        }
+
+    }while(idProductoIngresado.toUpperCase() != "VOLVER");
+    
 }
 
-function mostrarCarrito(precio){
+function eliminardelcarrito(idProducto){
 
-    let pago=prompt("El total del monto a pagar es: "+precio+"\nIndique forma de pago:\n -1: Pago total en efectivo\n -2: 3 cuotas\n -3: 6 cuotas \n -4: 12 cuotas");
+  let indice = carrito.findIndex((producto) => producto.id === idProducto);
+
+  if (indice === -1) {
+    return alert(`El producto no existe en el carrito`);
+  }else{
+
+    const confirmacion = confirm(`Estas seguro que deseas eliminar el producto ${carrito.nombre} del carrito?`)
+
+    if(confirmacion) {
+      carrito = carrito.filter( producto => producto.id !== idProducto); 
+    } else {
+      alert("Eliminación cancelada")
+    }
+
+  }
+
+}
+
+function mostrarCarrito(){
+   
+    let encendido = true;   
+
+    alert("Estos son los productos que agrego al carrito");
 
     
-    if(pago=="2"){
-        calcularCuotas(precio,3);
-    }else if(pago=="3"){
-        calcularCuotas(precio,6);
-    }else if(pago="4"){
-        calcularCuotas(precio,12);
+
+    do{
+
+      mostrarProductos(carrito,"carrito");
+
+      let opcion = prompt("Ingrese\n 1 - Para eliminar 1 producto del carrito\n 2- Para seguir comprando\n 3- Para ir a pagar");
+
+      switch(opcion){
+        case "1":
+          let idProducto = parseInt(prompt("Ingrese el id del producto a eliminar"));
+          eliminardelcarrito(idProducto);
+          break;
+        case "2":
+          mostrarMenuCategorias();
+          return;
+          break;
+        case "3":
+          encendido = false;
+          break;
+      }
+    }while(encendido === true);
+
+    let precio = carrito.reduce((contador,producto) => contador + producto.precio,0);
+
+    alert(`Debe abonar $${precio}`);
+
+}
+
+
+
+function mostrarMenuCategorias(){
+  
+    let encendido = true;
+
+   do{
+    let opcion = prompt("Ingrese\n 1 - Para ir a la seccion Camperas\n 2 - Para ir a la seccion Buzos\n 3- Para ir a la seccion Chalecos\n SALIR para ir al carrito");
+        switch (opcion.toUpperCase()) {
+            case "1":
+              mostrarMenuProductos("Camperas");
+              break;
+            case "2":
+              mostrarMenuProductos("Buzos");
+              break;
+            case "3":
+              mostrarMenuProductos("Chalecos");
+              break;
+            case "SALIR":
+              encendido = false;
+              break;
+            default:
+              alert("Inserte una opción correcta");
+        }
+    } while(encendido === true);
+    
+    if(carrito.length >  0){
+      mostrarCarrito();
     }else{
-        alert("El precio a pagar es $"+precio);
+      alert("No ha agregado ningun producto al carrito de compras");
     }
-
 }
 
-mostrarMenuProductos();
-if(cantidadProductos>0){
-    mostrarCarrito(precio);
-}else{
-    alert("No ha agregado ningun producto al carrito de compras");
-}
+
+mostrarMenuCategorias();
+
 
 alert("Muchas gracias por pasarse por nuestra tienda para volver a comprar debe recargar la pagina");
 
